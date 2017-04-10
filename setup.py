b1850af4fb2377973791317c16621f739b624550
Jakub Wilk <jwilk@jwilk.net>
2017-04-10 20:36:25 +0200

    setup: add.

---
+++ setup.py
@@ -0,0 +1,49 @@
+# encoding=UTF-8
+
+# Copyright © 2013-2017 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import distutils.core
+import distutils644
+
+classifiers = '''
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+'''.strip().splitlines()
+
+distutils.core.setup(
+    name='distutils644',
+    version='0.1',
+    license='MIT',
+    description='Python: monkey-patch distutils to sanitize ownership, permissions, tar format',
+    long_description=distutils644.__doc__.strip(),
+    classifiers=classifiers,
+    url='https://github.com/jwilk/distutils644',
+    author='Jakub Wilk',
+    author_email='jwilk@jwilk.net',
+    py_modules=['distutils644']
+)
+
+# vim:ts=4 sts=4 sw=4 et
