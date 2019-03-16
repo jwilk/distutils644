cd04ce774c1c74670da5495b3149209d83df63a4
Jakub Wilk <jwilk@jwilk.net>
2019-03-16 19:23:24 +0100

    setup: wrap very long line.

--- setup.py
+++ setup.py
@@ -96,7 +96,10 @@ distutils.core.setup(
     name='distutils644',
     version=get_version(),
     license='MIT',
-    description='normalize ownership, permissions, order of directory entries and tar format in distutils-generated archives',
+    description=(
+        'normalize ownership, permissions, order of directory entries '
+        'and tar format in distutils-generated archives'
+    ),
     long_description=__doc__.strip(),
     classifiers=classifiers,
     url='http://jwilk.net/software/distutils644',
